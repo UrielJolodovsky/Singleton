16318ebc88c04b43a47210ac3e523a6354bb40a1
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-10-13 09:00:49 -0300

    Initial commit

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] string[] sceneNames;
+    [SerializeField] int sceneindex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ChangeScenesSequentially()
+    {
+        if (sceneindex >= sceneNames.Length)
+        {
+            sceneindex = 0;
+        }
+        ChangeSceneAndIncrementIndex();
+    }
+    void ChangeSceneAndIncrementIndex()
+    {
+        SceneManager.LoadScene(sceneNames[sceneindex]);
+        sceneindex++;
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI txtScore;
+    public int scorePoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scorePoints = 0;
+        ScoreUpdate();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+    public void AddPoints(int points)
+    {
+        scorePoints += points;
+        ScoreUpdate();
+    }
+    public void ScoreUpdate()
+    {
+        txtScore.text = scorePoints.ToString();
+    }
+}
